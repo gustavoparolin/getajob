8eac17639d35c626beac7786089cd3ee2e47720e
Gustavo Parolin <gustavo@parolin.net>
2018-02-11 20:10:36 -0800

    Move Cards functions to a component; transform Add Card in a toggle button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -50,17 +50,19 @@ import App from '../app.vue'
 //     }
 //   }
 // })
+//
+window.store = {}
+
 document.addEventListener("turbolinks:load", function() {
 // document.addEventListener('DOMContentLoaded', function() {
   var element = document.querySelector("#boards")
   if (element != undefined) {
+    window.store.lists = JSON.parse(element.dataset.lists)
     // window.store.state.lists = JSON.parse(element.dataset.lists)
 
     const app = new Vue({
       el: element,
-      data: {
-        lists: JSON.parse(element.dataset.lists)
-      },
+      data: window.store,
       // store: window.store,
       template: "<App :original_lists='lists' />",
       components: { App }
